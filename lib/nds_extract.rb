4107bfc81bec061437be3688de93bc224984a28f
Wesley Chen <weschen1996@gmail.com>
2020-01-18 05:37:49 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,8 +2,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  def addTohash
-    
+  hash = {};
   
   nds[count][:movies][count2][:worldwide_gross]
   
