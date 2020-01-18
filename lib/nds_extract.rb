bf5ebad9f5d8f5e34ff82d4b928be22468397ba0
Wesley Chen <weschen1996@gmail.com>
2020-01-18 05:40:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,7 +5,7 @@ def directors_totals(nds)
   hash = {};
   dircount = 0;
   while nds[dircount] do
-    nds[dircount]
+    nds[dircount][:name]
     
   nds[count][:movies][count2][:worldwide_gross]
   
